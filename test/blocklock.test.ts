1a676d826b73e879a31e7b8eba6f9921cfa66989
Nemitari Ajienka <nemitariajienka@gmail.com>
2025-01-17 15:46:04 +0000

    update unit tests

--- test/blocklock.test.ts
+++ test/blocklock.test.ts
@@ -4,8 +4,9 @@ import {Blocklock} from "../src"
 
 describe("encryption", () => {
     it("class can be constructed", () => {
-        const w = new Wallet("0x5cb3c5ba25c91d84ef5dabf4152e909795074f9958b091b010644cb9c30e3203")
-        const e = new Blocklock(w)
-        expect(e).not.toEqual(null)
+        const wallet = new Wallet("0x5cb3c5ba25c91d84ef5dabf4152e909795074f9958b091b010644cb9c30e3203")
+        const blocklockContractAddress = "0xfd1bf3fcbf2e250abff4a61670dfa3ce740453e5"
+        const blocklockjs = new Blocklock(wallet, blocklockContractAddress)
+        expect(blocklockjs).not.toEqual(null)
     })
 })
